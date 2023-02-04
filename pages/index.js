07b00f3911c3578766ad2aae366431d5d1172195
Samarth <splangote_b20@ce.vjti.ac.in>
2023-02-04 19:26:10 +0530

    body tag changed

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Home() {
   const router = useRouter();
   return (
-    <div>
+    <div className='mainbody'>
         <button type='button' className="p-4 text-black" onClick={() => router.push('/user')}>User</button>
         <button type='button' className="p-4 text-black" onClick={() => router.push('/admin')} >Admin</button>
     </div>
